c2060f4ca1b8160ef7066c1ce2fbd2af915a651e
unknown <kbp4154@naver.com>
2021-07-12 10:14:28 +0900

    7.12 CodeLabPhotoLog Finish

--- CodelabPhotoLog/navigators/AppNavigator.js
+++ CodelabPhotoLog/navigators/AppNavigator.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import List from '../pages/List';
-import Form from '../pages/Form';
 import View from '../pages/View';
-
+import Form from '../pages/Form';
+import Settings from '../pages/Settings';
 
 const Stack = createStackNavigator();
 
-
 function AppNavigator(props) {
-    return (
+    return(
         <Stack.Navigator>
             <Stack.Screen name="List" component={List} />
             <Stack.Screen name="View" component={View} />
-            <Stack.Screen name="Form" component={Form}/>
+            <Stack.Screen name="Form" component={Form} />
+            <Stack.Screen name="Settings" component={Settings} />
         </Stack.Navigator>
     )
-
 }
 
 export default AppNavigator;
--- CodelabPhotoLog/pages/List.js
+++ CodelabPhotoLog/pages/List.js
@@ -1,20 +1,72 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
+import storage from '../net/storage';
+import { withContext } from 'context-q';
+import moment from 'moment';
 
 const Title = styled.Text`
     font-size: 36px;
 `;
-
 const Button = styled.Button``;
+const ListItem = styled.TouchableOpacity`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom-width: 1px;
+    border-bottom-color: #e5e5e5;
+`;
+const Row = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+const Thumbnail = styled.Image`
+    width: 80px;
+    height: 80px;
+    margin-right: 12px;
+`;
+const Tags = styled.Text``;
+const Date = styled.Text`
+    color: #aaaaaa;
+    margin-right: 12px;
+`;
 
 function Component(props) {
-    return (
+    const [list, setList] = React.useState([]);
+    React.useEffect(()=>{
+        const unsubscribe = props.navigation.addListener('focus', e => {
+            storage.readAll().then(data=>setList(data));
+        });
+        storage.readAll().then(data=>setList(data));
+        return () => unsubscribe();
+    },[]);
+    return(
         <>
-            <Title>작성</Title>
-
-            <Button title="조회" noPress
+            {list.map(item=>(
+                <ListItem key={ item.id } onPress={() => {
+                    props.navigation.navigate( 'View', { id: item.id } );
+                }}>
+                    <Row>
+                        <Thumbnail source={{ uri: item.url }}/>
+                        <Tags>
+                            {item.hashtags}
+                            
+                        </Tags>
+                    </Row>
+                    { props.context.showDate && (
+                        <Date>{ moment( item.id, 'x' ).format('YYYY-MM-DD') }</Date>
+                    ) }
+                </ListItem>
+            ))}
+            <Button title="사진 추가" onPress={() => {
+                props.navigation.navigate( 'Form' );
+            } }/>
+            <Button title="설정" onPress={() => {
+                props.navigation.navigate( 'Settings' );
+            } }/>
         </>
     )
 }
+
+Component = withContext( Component );
 
 export default Component;
